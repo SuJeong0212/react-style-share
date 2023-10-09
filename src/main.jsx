82d4191b68e9a551398555ee0a33b729c11b49d0
Sujeong0212 <s.rystal0212@gmail.com>
2023-10-09 16:36:34 +0900

    Init : create jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import Layout from './layouts';
+import Home from './pages/home';
+import ErrorPage from './pages/ErrorPage';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>,
-)
+);
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <div>
+      <p>페이지 오류</p>
+    </div>
+  );
+}
